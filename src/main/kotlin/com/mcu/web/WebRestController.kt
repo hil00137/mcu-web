4b6b1f6a73ed073152baa5345fbef26af3355f47
hil00137 <hil00137@gmail.com>
2019-11-18 22:20:17 +0900

    Initial commit

---
+++ src/main/kotlin/com/mcu/web/WebRestController.kt
@@ -0,0 +1,12 @@
+package com.mcu.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class WebRestController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "HelloWorld"
+    }
+}
