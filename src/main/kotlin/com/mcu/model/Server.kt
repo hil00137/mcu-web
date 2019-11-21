f549d0d0e3c42a6512e9ac2ff3b56adb335b5c39
hil00137 <hil00137@gmail.com>
2019-11-22 01:01:41 +0900

    Add IP address. refs #3

--- src/main/kotlin/com/mcu/model/Server.kt
+++ src/main/kotlin/com/mcu/model/Server.kt
@@ -10,6 +10,7 @@ class Server {
 
     @Id lateinit var id : String
     @Field lateinit var name : String
+    @Field lateinit var ip : String
     @Field var aws : Aws = Aws()
     @Field var minecraft : Minecraft = Minecraft()
 
