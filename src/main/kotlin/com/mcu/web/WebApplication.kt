bee00c316018ae2d80208af1bcccc5a547c41105
hil00137 <hil00137@gmail.com>
2019-11-21 23:11:53 +0900

    Add some control method for mongodb. fixes #2

--- src/main/kotlin/com/mcu/web/WebApplication.kt
+++ src/main/kotlin/com/mcu/web/WebApplication.kt
@@ -1,9 +1,11 @@
 package com.mcu.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [MongoAutoConfiguration::class, MongoDataAutoConfiguration::class], scanBasePackages = ["com.mcu.*"])
 class WebApplication
 
 fun main(args: Array<String>) {
