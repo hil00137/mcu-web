d4dd362b2d86198e0cd4722bca167a382cbd6108
hil00137 <hil00137@gmail.com>
2019-12-01 20:58:43 +0900

    Add ehcache dependency. refs #25

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.mcu"
-version = "0.2.3-SNAPSHOT"
+version = "0.2.4-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
@@ -29,6 +29,8 @@ dependencies {
     implementation("com.amazonaws:aws-java-sdk-ec2:1.11.675")
     implementation("org.springframework:spring-aop")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("net.sf.ehcache:ehcache:2.9.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
