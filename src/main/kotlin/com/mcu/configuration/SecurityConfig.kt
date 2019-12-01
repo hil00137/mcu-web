823e888d19c736b4854d504c00160a8198ce8160
hil00137 <hil00137@gmail.com>
2019-12-01 19:26:27 +0900

    Add css, js files and security. refs #13

--- src/main/kotlin/com/mcu/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/mcu/configuration/SecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 
@@ -33,7 +34,8 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
         http?:return
         http.authorizeRequests()
-                .antMatchers("/","/user/login","/error**","/css/**","/js/**").permitAll()
+                .antMatchers("/","/user/login","/user/signUp","/error**","/static/**","/js/**","/css/**").permitAll()
+                .antMatchers("/user/check**/**").permitAll()
                 .antMatchers("/**").access("ROLE_USER")
                 .antMatchers("/**").access("ROLE_ADMIN")
                 .antMatchers("/**").authenticated()
