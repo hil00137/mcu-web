c6bb7167e2ed89b6101a0b94067ad140aecd3b76
hil00137 <hil00137@gmail.com>
2019-12-01 21:01:57 +0900

    Add cache for user. fixed #25

--- src/main/kotlin/com/mcu/service/UserService.kt
+++ src/main/kotlin/com/mcu/service/UserService.kt
@@ -4,6 +4,7 @@ import com.mcu.model.User
 import com.mcu.repository.UserRepository
 import com.mcu.util.HashUtil
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,7 +15,8 @@ class UserService {
 
     fun getUserById(userId : String) = userRepository.findUserByUserId(userId)
     fun getUserByNickname(nickname : String) = userRepository.findUserByNickname(nickname)
-    fun registUser(user: User): User? {
+    @CacheEvict(value = ["userCache"], allEntries = true)
+    fun registerUser(user: User): User? {
         user.password = HashUtil.sha512(user.password)
         return userRepository.save(user)
     }
