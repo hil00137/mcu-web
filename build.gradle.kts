a9a4bae1780370ce34d4eaaa822f43c661d16f1d
hil00137 <hil00137@gmail.com>
2019-12-29 19:51:51 +0900

    Add Guide Video and Guide page. fixed #43

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.mcu"
-version = "0.3.4-RELEASE"
+version = "0.3.5-RELEASE"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,14 +1,24 @@
-new Vue({
+const headerVue = new Vue({
     el: '#header',
-    methods : {
-        logout : function() {
+    methods: {
+        logout: function () {
             const check = confirm("로그아웃 하시겠습니까?");
-            if(check) {
+            if (check) {
                 location.href = "/user/logout";
             }
         },
-        goHome: function() {
-            location.href ="/home";
+        goHome: function () {
+            location.href = "/home";
+        },
+        goGuide: function () {
+            location.href = "/guide";
         }
+    },
+    mounted: function () {
+        const url = window.location.href;
+        const index = url.lastIndexOf("/");
+        const current = url.substring(index + 1);
+        const nav = document.getElementById(current + "Header").parentElement;
+        nav.classList.add("active");
     }
 });
