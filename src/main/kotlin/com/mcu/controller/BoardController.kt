e2eb875032cde3a7602f98b180393304d6ead796
hil00137 <hil00137@gmail.com>
2020-01-05 21:25:49 +0900

    Make class BoardController and move method in WebCommonController about board. refs #58

--- src/main/kotlin/com/mcu/controller/BoardController.kt
+++ src/main/kotlin/com/mcu/controller/BoardController.kt
@@ -1,104 +1,56 @@
 package com.mcu.controller
 
-import com.mcu.model.Board
 import com.mcu.model.BoardType
-import com.mcu.model.History
 import com.mcu.service.BoardService
-import com.mcu.service.HistoryService
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cache.annotation.Cacheable
-import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
-import java.lang.IllegalArgumentException
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletRequest
 
-@RestController
 @RequestMapping("/board")
+@Controller
 class BoardController {
 
-    val logger = LoggerFactory.getLogger(this::class.java)!!
-
     @Autowired
-    lateinit var boardService: BoardService
+    lateinit var boardService : BoardService
 
-    @Autowired
-    lateinit var historyService : HistoryService
 
     /**
-     * 정해진 게시판의 page별로 게시글들을 가져옴. Caching
+     * board detail page
      */
-    @Cacheable(value = ["boardCache"])
-    @GetMapping("/{type}/{page}")
-    fun getBoards(@PathVariable type : String, @PathVariable page : String) : Map<String, Any> {
-        logger.info("Get Boards type: $type, page : $page")
-        var boardType: BoardType? = null
-        var pageNum: Int
-        try {
-            boardType = BoardType.valueOf(type.toUpperCase())
-            pageNum = page.toInt()
-        } catch (e : IllegalArgumentException) {
-            return HashMap()
-        } catch (e : NumberFormatException) {
-            pageNum = 0
+    @GetMapping("/detail/{id}")
+    fun goBoardDetail(@PathVariable id : String) : ModelAndView {
+        var targetBoard = boardService.getBoardById(id)
+        if(targetBoard != null) {
+            val userId = SecurityContextHolder.getContext().authentication.principal as String
+            boardService.hitCount(targetBoard, userId)
         }
+        return ModelAndView("board/detail").addObject("board", targetBoard)
+    }
 
-        val result = HashMap<String, Any>()
-        result["count"] = boardService.getCountOfBoard(boardType!!)
-        result["list"] = boardService.getBoards(boardType, pageNum)
-        result["page"] = pageNum
-        return result
+    @GetMapping("/write")
+    fun goWrite(): String {
+        return "board/write"
+    }
+
+    @GetMapping("")
+    fun goBoardList(request : HttpServletRequest): ModelAndView {
+        val boardType = request.getParameter("boardType")
+        return ModelAndView("board/list").addObject("boardType",boardType?: BoardType.NOTIFICATION.type)
     }
 
     /**
-     * 게시글 등록
+     * 수정하기 위해 write page로
      */
-    @PostMapping("/{type}")
-    fun saveBoard(@PathVariable type: String, @RequestBody board: Board?) : Map<String, Any> {
-        val boardType: BoardType?
-        val result = HashMap<String, Any>()
-        try {
-            boardType = BoardType.valueOf(type.toUpperCase())
-        } catch (e : IllegalArgumentException) {
-            result["message"] = "존재하지 않는 게시판입니다."
-            return result
+    @PostMapping("/write")
+    fun goModifyBoard(@ModelAttribute("boardId") boardId : String) : ModelAndView {
+        if ("" == boardId) {
+            return ModelAndView("board/write").addObject("message","잘못된 입력값입니다.")
         }
-        val userId = SecurityContextHolder.getContext().authentication.principal as String
-        if(board == null) {
-            result["message"]="잘못된 접근입니다."
-            return result;
-        } else if (boardType == BoardType.NOTIFICATION) {
-            if(!SecurityContextHolder.getContext().authentication.authorities.contains(SimpleGrantedAuthority("ROLE_ADMIN"))) {
-                result["message"]="잘못된 접근입니다."
-                historyService.writeHistory("Access is not allowed.[save board] from $userId", History.RULE_OVER)
-                return result;
-            }
-        }
-        board.userId = userId
-        val boardId = boardService.saveBoard(boardType, board).id
-        result["message"] = "success"
-        result["boardId"] = boardId
-        return result
-    }
+        val board = boardService.getBoardById(boardId) ?: return ModelAndView("board/write").addObject("message","잘못된 입력값입니다.")
 
-    /**
-     * 게시글 삭제기능
-     */
-    @DeleteMapping("/{id}")
-    fun deleteBoard(@PathVariable id : String?) : String {
-        if (id == null || "" == id) {
-            return "잘못된 입력값입니다."
-        }
-        val board = boardService.getBoardById(id) ?: return "잘못된 입력값입니다."
-        val requestId = SecurityContextHolder.getContext().authentication.principal as String
-        return if (board.userId == requestId ||
-                SecurityContextHolder.getContext().authentication.authorities.contains(SimpleGrantedAuthority("ROLE_ADMIN"))) {
-            boardService.deleteBoard(board)
-            historyService.writeHistory("delete board id : $id from $requestId", History.USER_REQUEST)
-            "삭제하였습니다."
-        } else {
-            historyService.writeHistory("Access is not allowed.[delete board] from $requestId", History.RULE_OVER)
-            "권한이 없습니다."
-        }
+        return ModelAndView("board/write").addObject("message","success").addObject("board", board)
     }
 }
--- src/main/kotlin/com/mcu/controller/WebCommonController.kt
+++ src/main/kotlin/com/mcu/controller/WebCommonController.kt
@@ -1,14 +1,9 @@
 package com.mcu.controller
 
-import com.mcu.model.BoardType
 import com.mcu.service.BoardService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.servlet.ModelAndView
-import javax.servlet.http.HttpServletRequest
 
 @Controller
 class WebCommonController {
@@ -24,29 +19,5 @@ class WebCommonController {
     @GetMapping("/guide")
     fun guide(): String {
         return "guide"
-    }
-
-    /**
-     * board detail page
-     */
-    @GetMapping("/board/detail/{id}")
-    fun goBoardDetail(@PathVariable id : String) : ModelAndView {
-        var targetBoard = boardService.getBoardById(id)
-        if(targetBoard != null) {
-            val userId = SecurityContextHolder.getContext().authentication.principal as String
-            boardService.hitCount(targetBoard, userId)
-        }
-        return ModelAndView("board/detail").addObject("board", targetBoard)
-    }
-
-    @GetMapping("/board/write")
-    fun goWrite(): String {
-        return "board/write"
-    }
-
-    @GetMapping("/board")
-    fun goBoardList(request : HttpServletRequest): ModelAndView {
-        val boardType = request.getParameter("boardType")
-        return ModelAndView("board/list").addObject("boardType",boardType?:BoardType.NOTIFICATION.type)
     }
 }
