1dd22d1504dcc4123b77737c049a21ecaf7bead9
hil00137 <hil00137@gmail.com>
2020-01-22 00:46:02 +0900

    Release 0.5.2 for modifying user info. fixed #82

--- src/main/kotlin/com/mcu/service/UserService.kt
+++ src/main/kotlin/com/mcu/service/UserService.kt
@@ -1,5 +1,6 @@
 package com.mcu.service
 
+import com.mcu.model.HistoryPriority
 import com.mcu.model.Mail
 import com.mcu.model.User
 import com.mcu.repository.UserRepository
@@ -25,6 +26,9 @@ class UserService {
     @Autowired
     private lateinit var userRepository : UserRepository
 
+    @Autowired
+    private lateinit var historyService : HistoryService
+
     @Cacheable(value = ["userCache"], key = "'userId:' + #userId")
     fun getUserByUserId(userId : String) = userRepository.findUserByUserId(userId)
     @Cacheable(value = ["userCache"], key = "'nickname:' + #nickname")
@@ -32,14 +36,47 @@ class UserService {
     @Cacheable(value = ["userCache"], key = "'email:' + #email")
     fun getUserByEmail(email : String) = userRepository.findUserByEmail(email)
 
+    fun update(user: User) = userRepository.save(user)!!
+
+    fun sendEmailChangeMail(user : User, oriEmail : String): User? {
+        user.mailAuthCode = UUID.randomUUID().toString().replace("-","").substring(0,9)
+        val ip = this.getIp()
+        Thread {
+            val mail = Mail(user)
+            mail.subject = "마크대학 이메일 인증메일입니다"
+            mail.setEmailChangeContent(ip, homepageUrl, user)
+            val resultMap = mailSendUtil.sendEmail(mail)
+            val targetUser = this.getUserByUserId(user.userId?:"")?:User()
+
+            if(resultMap["result"] == "success") {
+                targetUser.mailAuth = "wait"
+            } else if(resultMap["result"] == "fail") {
+                val failEmail = targetUser.email
+                historyService.writeHistoryAsAdmin("Email Change Fail $oriEmail -> $failEmail", HistoryPriority.ERROR)
+                targetUser.mailAuth = "success"
+                targetUser.email = oriEmail
+                this.errorEmailNotify(targetUser, failEmail?:"", ip)
+            }
+            userRepository.save(targetUser)
+        }.start()
+        return userRepository.save(user)
+    }
+
+    fun errorEmailNotify(user: User, failEmail : String, ip : String) {
+        Thread {
+            val mail = Mail(user)
+            mail.subject = "인증메일 전송에 실패하였습니다"
+            mail.setEmailChangeFailContent(ip, failEmail)
+            mailSendUtil.sendEmail(mail)
+        }.start()
+    }
+
     fun registerUser(user: User): User? {
         user.password = HashUtil.sha512(user.password)
         user.mailAuthCode = UUID.randomUUID().toString().replace("-","").substring(0,9)
         val ip = this.getIp()
         Thread {
-            val mail = Mail()
-            mail.toMail = user.email!!
-            mail.toName = user.nickname!!
+            val mail = Mail(user)
             mail.subject = "마크대학 이메일 인증메일입니다"
             mail.setEmailAuthContent(ip, homepageUrl, user)
             val resultMap = mailSendUtil.sendEmail(mail)
