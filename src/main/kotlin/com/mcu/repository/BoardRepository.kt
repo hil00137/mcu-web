4027e81cce6084c1b1affdd498f4ff2ea323ebcd
hil00137 <hil00137@gmail.com>
2020-01-25 13:27:10 +0900

    Divide CacheEvict for efficiency and Add countByUserId and findByUserIdWithPage. refs #67

--- src/main/kotlin/com/mcu/repository/BoardRepository.kt
+++ src/main/kotlin/com/mcu/repository/BoardRepository.kt
@@ -10,6 +10,7 @@ import com.mcu.util.AwsConnector
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.Caching
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -18,13 +19,23 @@ class BoardRepository {
     @Autowired
     private lateinit var awsConnector : AwsConnector
 
-    @CacheEvict(value = ["boardCache"], allEntries = true)
+    @Caching(evict = [
+        CacheEvict(cacheNames = ["boardCache"], key = "'typeCount:'+#item.type"),
+        CacheEvict(cacheNames = ["boardCache"], key = "'userIdCount:'+#item.userId"),
+        CacheEvict(cacheNames = ["boardCache"], key = "'id:'+#item.id?:''"),
+        CacheEvict(cacheNames = ["boardListCache"], allEntries = true)
+    ])
     fun save(item: Board) : Board {
         awsConnector.getDynamoDBMapper().save(item)
         return awsConnector.getDynamoDBMapper().load(item)
     }
-    
-    @CacheEvict(value = ["boardCache"], allEntries = true)
+
+    @Caching(evict = [
+        CacheEvict(cacheNames = ["boardCache"], key = "'typeCount:'+#item.type"),
+        CacheEvict(cacheNames = ["boardCache"], key = "'userIdCount:'+#item.userId"),
+        CacheEvict(cacheNames = ["boardCache"], key = "'id:'+#item.id"),
+        CacheEvict(cacheNames = ["boardListCache"], allEntries = true)
+    ])
     fun delete(item : Board) {
         awsConnector.getDynamoDBMapper().delete(item)
     }
@@ -41,6 +52,13 @@ class BoardRepository {
         return awsConnector.getDynamoDBMapper().count(Board::class.java,queryExpression)
     }
 
+    @Cacheable(value = ["boardCache"], key = "'userIdCount:'+#userId")
+    fun countByUserId(userId : String) : Int {
+        val queryExpression = DynamoDBQueryExpression<Board>()
+        queryExpression.withIndexName("Board-userId-regist").withConsistentRead(false).withHashKeyValues(Board().also { it.userId = userId })
+        return awsConnector.getDynamoDBMapper().count(Board::class.java,queryExpression)
+    }
+
     @Cacheable(value = ["boardCache"], key = "'id:'+#id")
     fun findById(id : String) : Board? {
         return try {
@@ -54,6 +72,17 @@ class BoardRepository {
     fun findByTypeWithPage(type: String, last : MutableMap<String, AttributeValue>): QueryResultPage<Board>? {
         val queryExpression = DynamoDBQueryExpression<Board>()
         queryExpression.withIndexName("Board-type-regist").withConsistentRead(false).withHashKeyValues(Board().also { it.type = type })
+                .withLimit(10).withScanIndexForward(false)
+        if (last.isNotEmpty()) {
+            queryExpression.withExclusiveStartKey(last)
+        }
+        return  awsConnector.getDynamoDBMapper().queryPage(Board::class.java, queryExpression).let { McuQueryResultPage(it) }
+    }
+
+    @Cacheable(value = ["boardListCache"], key = "'userId:'+ #userId+':'+#last")
+    fun findByUserIdWithPage(userId: String, last : MutableMap<String, AttributeValue>): QueryResultPage<Board>? {
+        val queryExpression = DynamoDBQueryExpression<Board>()
+        queryExpression.withIndexName("Board-userId-regist").withConsistentRead(false).withHashKeyValues(Board().also { it.userId = userId })
                 .withLimit(10).withScanIndexForward(false)
         if (last.isNotEmpty()) {
             queryExpression.withExclusiveStartKey(last)
